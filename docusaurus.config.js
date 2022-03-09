2666b00cdd6c8ee699b8f5ea3a5ceaf73bf2fe57
anchelte <admin@obsly.host>
2022-03-09 20:47:24 +0100

    main

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -65,7 +65,56 @@ const config = {
           },
         ],
       },
-      
+            footer: {
+        style: 'dark',
+        links: [
+          {
+            title: 'Docs',
+            items: [
+              {
+                label: 'Billing',
+                to: '/billing',
+              },
+              {
+                label: 'Gameservers',
+                to: '/gameservers',
+              },
+            ],
+          },
+          {
+            title: 'Gameservers',
+            items: [
+              {
+                label: 'Stack Overflow',
+                href: 'https://stackoverflow.com/questions/tagged/docusaurus',
+              },
+              {
+                label: 'Discord',
+                href: 'https://discordapp.com/invite/docusaurus',
+              },
+              {
+                label: 'Twitter',
+                href: 'https://twitter.com/docusaurus',
+              },
+            ],
+          },
+          {
+            title: 'More',
+            items: [
+              {
+                label: 'Blog',
+                to: '/billing',
+              },
+              {
+                label: 'GitHub',
+                href: 'https://github.com/facebook/docusaurus',
+              },
+            ],
+          },
+        ],
+        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+      },
+
       prism: {
         theme: lightCodeTheme,
         darkTheme: darkCodeTheme,
